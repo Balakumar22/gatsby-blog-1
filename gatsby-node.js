b83faedf12b1a18e46015f912222ff86981542d3
Balakumar A N <balakumar.arunbala@gmail.com>
2023-01-02 01:04:53 +0530

    Add posts in heading

--- gatsby-node.js
+++ gatsby-node.js
@@ -16,3 +16,47 @@ exports.createPages = async ({ actions }) => {
     defer: true,
   })
 }
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve(`./src/templates/blog-post.js`),
+        context: {
+          slug: node.fields.slug,
+        },
+      })
+    })
+  })
+}
+
+const { createFilePath } = require(`gatsby-source-filesystem`)
+const path = require("path")
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode })
+
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
